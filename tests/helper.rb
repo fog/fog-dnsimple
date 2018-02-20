3b4f7235ea28f193eb3adab1c79c407568a8debc
Simone Carletti <weppos@weppos.net>
2018-02-20 12:23:53 +0100

    No longer using CodeClimate on this project

--- tests/helper.rb
+++ tests/helper.rb
@@ -1,10 +1,3 @@
-begin
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-rescue LoadError => e
-  $stderr.puts "not recording test coverage: #{e.inspect}"
-end
-
 require 'fog/test_helpers'
 require File.expand_path('../../lib/fog/dnsimple', __FILE__)
 
